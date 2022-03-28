8f75876cef566a605a84e20dfd46ffdc8e12135c
anish <anishbasnet027@gmail.com>
2022-03-28 07:49:43 +0545

    addded mysql

--- fypbackend/settings.py
+++ fypbackend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'fypbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'batuwa',  
+        'USER': 'root',  
+        'PASSWORD': 'anish',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',
     }
 }
 
