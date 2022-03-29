74c4dd79f8dce6daf669f0faf2d1863434b826e0
anish <anishbasnet027@gmail.com>
2022-03-29 22:32:58 +0545

    working login signup with fe

--- fypbackend/settings.py
+++ fypbackend/settings.py
@@ -87,13 +87,15 @@ DATABASES = {
     }
 }
 
-# REST_FRAMEWORK = {
+ALLOWED_HOSTS = ['*']
 
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',
-#     )
+REST_FRAMEWORK = {
 
-# }
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
